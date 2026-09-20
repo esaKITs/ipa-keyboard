243d061bfe1a2ef1f08c27c4c359571928f1fa2d
Tetsuya ESAKI <esakit@sage.ocn.ne.jp>
2026-09-21 03:08:21 +0900

    Refresh offline cache for font-independent build

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='ipa-keyboard-v1';
-const CORE=['./','./index.html'];
+const CACHE='ipa-keyboard-v2';
+const CORE=['./','./index.html','./licenses/DoulosSIL-OFL.txt'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
